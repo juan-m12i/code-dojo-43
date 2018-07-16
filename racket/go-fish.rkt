a002b4a562420620efbd8fb7e4c29ac48f4d089a
Nigel Runnels-Moss <sleepyfox@gmail.com>
2018-07-16 21:17:13 +0100

    Wrap tests in a test suite

--- racket/go-fish.rkt
+++ racket/go-fish.rkt
@@ -1,9 +1,14 @@
 #lang racket/base
 
-(require rackunit)
+(require rackunit rackunit/text-ui)
 
-(test-case
- "Check unit tests work"
- (let ([plus +][mult *])
-   (check-equal? (plus 41 1) 42 "Simple addition")
-   (check-equal? (mult 6 9) 42 "Simple multiplication")))
+(define game-tests
+  (test-suite
+   "Go Fish! game tests"
+   (test-case
+    "Check unit tests work"
+    (let ([plus +][mult *])
+      (check-equal? (plus 41 1) 42 "Simple addition")
+      (check-equal? (mult 6 9) 42 "Simple multiplication")))))
+
+(run-tests game-tests)
